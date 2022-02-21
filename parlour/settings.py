2419972143042f968f0d5d4993713eb3d8e1e299
Edwike12 <edwike.nyauncho@student.moringaschool.com>
2022-02-21 18:11:42 +0300

    created a .env and .gitignore files

--- parlour/settings.py
+++ parlour/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*i$x0yxcsf7vp^s^k*_48d)^&2d9*8(ubicfgh0r(7plsoivb('
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
