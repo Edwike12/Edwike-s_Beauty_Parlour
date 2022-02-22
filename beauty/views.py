78ccd48affb802aca496eeee2f18833a0441b1c3
Edwike12 <edwike.nyauncho@student.moringaschool.com>
2022-02-22 15:19:45 +0300

    create a profile and update_profile functions

--- beauty/views.py
+++ beauty/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from beauty.forms import *
+from beauty.models import *
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+@login_required()
+def profile(request):
+    current_user = request.user
+    profile = Profile.objects.filter(user_id=current_user.id).first()
+    return render(request, "profile.html", {"profile": profile, })
+
+
+@login_required(login_url='/accounts/login/')
+def update_profile(request, id):
+    user = User.objects.get(id=id)
+    profile = Profile.objects.get(user_id=user)
+    form = UpdateProfileForm(instance=profile)
+    if request.method == "POST":
+        form = UpdateProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+
+            profile = form.save(commit=False)
+            profile.save()
+            return redirect('profile')
+
+    return render(request, 'update_profile.html', {"form": form})    
