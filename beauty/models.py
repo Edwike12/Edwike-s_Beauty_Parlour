0642ded032a24925270566cf69d64e747b8e353d
Edwike12 <edwike.nyauncho@student.moringaschool.com>
2022-02-22 15:16:01 +0300

    add class profile and its fields in models.py

--- beauty/models.py
+++ beauty/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+#class profile
+class Profile(models.Model):
+    profile_photo=CloudinaryField('image')
+    name=models.TextField(max_length=50)
+    user=models.OneToOneField(User,on_delete=models.CASCADE, related_name='profile')
+    email=models.CharField(null=True, max_length=50)
+    phone_number=models.IntegerField(null=True)
+
+    @receiver(post_save , sender = User)
+    def create_profile(instance,sender,created,**kwargs):
+      if created:
+        Profile.objects.create(user = instance)
+
+    @receiver(post_save,sender = User)
+    def save_profile(sender,instance,**kwargs):
+      instance.profile.save()
+
+      def __str__(self):
+        return f'{self.user.username} profile'
