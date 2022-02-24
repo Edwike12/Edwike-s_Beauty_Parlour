7078702976e5520739f72bf3d9e8113bef7f64ee
Edwike12 <edwike.nyauncho@student.moringaschool.com>
2022-02-24 12:24:17 +0300

    add products,checkout,updates and cart  url paths

--- beauty/urls.py
+++ beauty/urls.py
@@ -5,5 +5,9 @@ urlpatterns=[
    path('',views.index,name = 'index'),
    path('profile/', views.profile, name='profile'),
    path('update_profile/<int:id>', views.update_profile, name='update_profile'),
+   path('products/', views.products, name='products'),
+   path('checkout/', views.checkout, name='checkout'),
+   path('updates/', views.updates, name='updates'),
+   path('cart/', views.cart, name='cart'),
 
 ]   
